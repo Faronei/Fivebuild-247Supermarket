057a3783d4ce67c80312ebbde0342af2f2424677
Francesco Mapping <francescobirp@gmail.com>
2025-02-05 15:32:21 +0100

    test
    test

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "bodacious"
+game "gta5"
+
+author "Francesco - discord: francesco441"
+description "MLO - 24/7 https://discord.gg/GYaJ5zanN9 "
+version "1.0.0"
+
+
+this_is_a_map "yes"
+
+
+lua54 'yes'
